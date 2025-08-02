4ef00fddc9d5ada13fa1d044f22564b3bb6ae697
Rafael Bagattini Guedes <129438343+rafaelbagattini@users.noreply.github.com>
2025-08-02 10:47:14 -0300

    Update install_speedtest.sh

--- install_speedtest.sh
+++ install_speedtest.sh
@@ -19,7 +19,7 @@ print_action() {
 print_info "Verificando pacotes speedtest e speedtest-cli..."
 if dpkg -l | grep -q speedtest; then
     print_action "Removendo pacote speedtest..."
-    sudo apt remove -y speedtest >/dev/null 2>&1
+     apt remove -y speedtest >/dev/null 2>&1
     print_success "speedtest removido com sucesso."
 else
     print_info "Pacote speedtest não instalado."
@@ -27,7 +27,7 @@ fi
 
 if dpkg -l | grep -q speedtest-cli; then
     print_action "Removendo pacote speedtest-cli..."
-    sudo apt remove -y speedtest-cli >/dev/null 2>&1
+     apt remove -y speedtest-cli >/dev/null 2>&1
     print_success "speedtest-cli removido com sucesso."
 else
     print_info "Pacote speedtest-cli n  o instalado."
@@ -37,7 +37,7 @@ fi
 print_info "Verificando arquivo de repositório speedtest.list..."
 if [ -f /etc/apt/sources.list.d/speedtest.list ]; then
     print_action "Removendo arquivo speedtest.list..."
-    sudo rm -f /etc/apt/sources.list.d/speedtest.list
+     rm -f /etc/apt/sources.list.d/speedtest.list
     print_success "Arquivo speedtest.list removido."
 else
     print_info "Arquivo speedtest.list não encontrado."
@@ -47,8 +47,8 @@ fi
 print_info "Verificando se o curl está instalado..."
 if ! command -v curl >/dev/null 2>&1; then
     print_action "Instalando curl..."
-    sudo apt update >/dev/null 2>&1
-    sudo apt install curl -y >/dev/null 2>&1
+     apt update >/dev/null 2>&1
+     apt install curl -y >/dev/null 2>&1
     print_success "curl instalado com sucesso."
 else
     print_info "curl já está instalado."
@@ -66,7 +66,7 @@ if [ -f /etc/apt/sources.list.d/ookla_speedtest-cli.list ]; then
     distro_codename=$(lsb_release -c | awk '{print $2}')
     if [ "$distro_codename" == "noble" ]; then
         print_action "Substituindo 'noble' por 'jammy' em speedtest.list..."
-        sudo sed -i 's/noble/jammy/g' /etc/apt/sources.list.d/ookla_speedtest-cli.list
+         sed -i 's/noble/jammy/g' /etc/apt/sources.list.d/ookla_speedtest-cli.list
         print_success "'noble' substituído por 'jammy'."
     else
         print_info "Não é necessário ajustar o arquivo: versão do Ubuntu    '$distro_codename'."
@@ -77,10 +77,10 @@ fi
 
 # Atualiza repositórios
 print_action "Atualizando lista de pacotes..."
-sudo apt update >/dev/null 2>&1
+apt update >/dev/null 2>&1
 print_success "Lista de pacotes atualizada."
 
 # Instala o speedtest
 print_action "Instalando o speedtest..."
-sudo apt install speedtest -y >/dev/null 2>&1
+apt install speedtest -y >/dev/null 2>&1
 print_success "speedtest instalado com sucesso!! \n"
