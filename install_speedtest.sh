96c617b6a3358aca5badaff5ba373d1dc9d5dd1c
Rafael Bagattini Guedes <129438343+rafaelbagattini@users.noreply.github.com>
2025-08-02 11:01:06 -0300

    Update install_speedtest.sh

--- install_speedtest.sh
+++ install_speedtest.sh
@@ -3,23 +3,22 @@ clear
 
 # Função para mensagens coloridas
 print_info() {
-    echo -e "\033[0;34m[INFO]\033[0m \n $1 \n"
+    echo -e "\033[0;34m\e[1m[INFO]\033[0m \n\e[1m $1 \033[0m\n"
 }
 print_success() {
-    echo -e "\e[32m[SUCCESSO]\033[0m \n $1 \n"
+    echo -e "\e[32m\e[1m[SUCCESSO]\033[0m \n\e[1m $1 \033[0m\n"
 }
 print_warning() {
-    echo -e "\e[33m[AVISO]\033[0m \n $1 \n"
+    echo -e "\e[33m\e[1m[AVISO]\033[0m \n\e[1m $1 \033[0m\n"
 }
 print_action() {
-    echo -e "\e[31m[AÇÃO]\033[0m \n $1 \n"
+    echo -e "\e[31m\e[1m[AÇÃO]\033[0m \n\e[1m $1 \033[0m\n"
 }
-
 # Verifica e remove speedtest & speedtest-cli
 print_info "Verificando pacotes speedtest e speedtest-cli..."
 if dpkg -l | grep -q speedtest; then
     print_action "Removendo pacote speedtest..."
-     apt remove -y speedtest >/dev/null 2>&1
+    apt remove -y speedtest >/dev/null 2>&1
     print_success "speedtest removido com sucesso."
 else
     print_info "Pacote speedtest não instalado."
@@ -27,7 +26,7 @@ fi
 
 if dpkg -l | grep -q speedtest-cli; then
     print_action "Removendo pacote speedtest-cli..."
-     apt remove -y speedtest-cli >/dev/null 2>&1
+    apt remove -y speedtest-cli >/dev/null 2>&1
     print_success "speedtest-cli removido com sucesso."
 else
     print_info "Pacote speedtest-cli n  o instalado."
@@ -37,7 +36,7 @@ fi
 print_info "Verificando arquivo de repositório speedtest.list..."
 if [ -f /etc/apt/sources.list.d/speedtest.list ]; then
     print_action "Removendo arquivo speedtest.list..."
-     rm -f /etc/apt/sources.list.d/speedtest.list
+    rm -f /etc/apt/sources.list.d/speedtest.list
     print_success "Arquivo speedtest.list removido."
 else
     print_info "Arquivo speedtest.list não encontrado."
@@ -47,8 +46,8 @@ fi
 print_info "Verificando se o curl está instalado..."
 if ! command -v curl >/dev/null 2>&1; then
     print_action "Instalando curl..."
-     apt update >/dev/null 2>&1
-     apt install curl -y >/dev/null 2>&1
+    apt update >/dev/null 2>&1
+    apt install curl -y >/dev/null 2>&1
     print_success "curl instalado com sucesso."
 else
     print_info "curl já está instalado."
@@ -62,14 +61,14 @@ print_success "Repositório adicionado com sucesso."
 # Verifica e ajusta arquivo de repositório speedtest.list
 print_info "Verificando arquivo de repositório speedtest.list..."
 if [ -f /etc/apt/sources.list.d/ookla_speedtest-cli.list ]; then
-    print_action "Verificando versão do Ubuntu..."
-    distro_codename=$(lsb_release -c | awk '{print $2}')
+    print_action "Verificando versão do Linux..."
+    distro_codename=$(source /etc/os-release && echo "$VERSION_CODENAME")
     if [ "$distro_codename" == "noble" ]; then
         print_action "Substituindo 'noble' por 'jammy' em speedtest.list..."
-         sed -i 's/noble/jammy/g' /etc/apt/sources.list.d/ookla_speedtest-cli.list
+        sed -i 's/noble/jammy/g' /etc/apt/sources.list.d/ookla_speedtest-cli.list
         print_success "'noble' substituído por 'jammy'."
-    else
-        print_info "Não é necessário ajustar o arquivo: versão do Ubuntu    '$distro_codename'."
+    #else
+    #    print_info "Não foi necessário ajustar o arquivo."
     fi
 else
     print_info "Arquivo speedtest.list não encontrado."
