d903be5cdad2a9fa2ff3b5b38e0aff7c7cd35e20
Rafael Bagattini Guedes <129438343+rafaelbagattini@users.noreply.github.com>
2025-08-06 19:07:33 -0300

    Teste de velocidade

--- speedtest.sh
+++ speedtest.sh
@@ -2,7 +2,7 @@
 cd /speedtest
 
 # Realiza o teste de velocidade
-speedtest -f json-pretty -s 42769 > resultado.txt
+speedtest -f json-pretty > resultado.txt
 
 # Baixa o print do resultado
 wget -qO print.png $(cat resultado.txt | jq '.result.url' -r).png 
