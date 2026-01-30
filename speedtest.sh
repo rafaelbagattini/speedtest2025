42549a3c1bdaa80c6e5e06e1f6514d6f63eba6e6
Rafael Bagattini Guedes <129438343+rafaelbagattini@users.noreply.github.com>
2026-01-30 11:36:01 -0300

    script com zabbix_sender

--- speedtest.sh
+++ speedtest.sh
@@ -2,10 +2,17 @@
 cd /speedtest
 
 # Realiza o teste de velocidade
-speedtest -f json-pretty > resultado.txt
+speedtest -f json-pretty > resultado.json
+
+# Envia o resultado.json para o Zabbix.
+# No parâmetro -s deve ser informado o "hostname" que está no Zabbix
+zabbix_sender -z 127.0.0.1 -s "Teste de velocidade" -k speedtest.resultado -o "$(cat resultado.json)"
 
 # Baixa o print do resultado
-wget -qO print.png $(cat resultado.txt | jq '.result.url' -r).png 
+wget -qO print.png $(cat resultado.json | jq '.result.url' -r).png 
 
 # Converte o print para base64
-base64 print.png > imgbase64.txt
+base64 print.png > img.b64
+
+#Envia o print para o Zabbix
+zabbix_sender -z 127.0.0.1 -s "Teste de velocidade" -k speedtest.print -o "$(cat img.b64)"
